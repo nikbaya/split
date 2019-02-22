6f19b617838c00d6eeb07c830c52c89c8609833a
Nik Baya <40802812+nikbaya@users.noreply.github.com>
2019-02-22 15:39:06 -0500

    Update gwas.py

--- gwas.py
+++ gwas.py
@@ -1,39 +1,6 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 16 16:19:00 2019
-
-Runs a GWAS on a matrix table, given the dependent/independent variables and
-list of covariates. 
-Options:    list of covariates for linreg, list of variables to pass through,
-            path to save
-
-
-@author: nbaya
-"""
-
-import hail as hl
-
-def normalize_genotypes(mt, genotype):
-    '''Normalizes genotypes'''
-    print('\rNormalizing genotypes...')
-    mt1 = mt.annotate_entries(__gt = genotype)
-    mt2 = mt1.annotate_rows(__gt_stats = hl.agg.stats(mt1.__gt))
-    return mt2.annotate_entries(__norm_gt = (mt2.__gt-mt2.__gt_stats.mean)/mt2.__gt_stats.stdev)   
-
-def gwas(mt, x, y, cov_list=[], with_intercept=True, normalize_x=True, pass_through=[],
-         path_to_save=None, is_std_cov_list=True):
+def gwas(mt, x, y, cov_list=[], with_intercept=True, pass_through=[], path_to_save=None, 
+         normalize_x=True, is_std_cov_list=False):
     '''Runs GWAS'''
-    header = '\n****************************************\n'
-    header += 'Running GWAS\n'
-    header += 'Covariates list: {}\n'.format(cov_list if not is_std_cov_list else 'Standard 26 covariates')
-    header += 'With intercept: {}\n'.format(with_intercept)
-    header += 'Normalizing X: {}\n'.format(normalize_x)
-    header += '' if pass_through is [] else 'Variables to pass through: {}\n'.format(pass_through)
-    header += '' if path_to_save is None else 'Saving to: {}\n'.format(path_to_save)
-    header += '****************************************'
-    print(header)
-    
     
     mt = mt._annotate_all(col_exprs={'y':y},
                            entry_exprs={'x':x})
@@ -49,25 +16,28 @@ def gwas(mt, x, y, cov_list=[], with_intercept=True, normalize_x=True, pass_thro
         cov_list = list(map(lambda x: mt[x] if type(x) is str else x,cov_list))
         
     cov_list = ([1] if with_intercept else [])+cov_list
+    
+    print(pass_through)
 
     gwas_ht = hl.linear_regression_rows(y=mt.y,
                                         x=mt.x,
                                         covariates=cov_list,
-                                        pass_through = pass_through)
+                                        pass_through = ['rsid']+pass_through)
+    
     gwas_ht = gwas_ht.annotate_globals(with_intercept = with_intercept)
+        
+    gwas_ht = gwas_ht.rename({'rsid':'SNP'}).key_by('SNP')
+        
+    gwas_ht = gwas_ht.select(Z = gwas_ht.t_stat,
+                             N = gwas_ht.n)
+    
+    sumstats_template = hl.import_table('gs://nbaya/rg_sex/50_snps_alleles_N.tsv.gz',types={'N': hl.tint64})
+    sumstats_template = sumstats_template.key_by('SNP')
+        
+    sumstats = sumstats_template.annotate(Z = gwas_ht[sumstats_template.SNP].Z,
+                                          N = gwas_ht[sumstats_template.SNP].N)
     
     if path_to_save is not None:
-        gwas_ht.write(path_to_save)
+        sumstats.export(path_to_save)
         
     return gwas_ht
-    
-if __name__ == '__main__':
-    gs_bucket = 'gs://nbaya/ldscsim/'
-    mt0 = hl.read_matrix_table(gs_bucket+'hm3.50_sim_h2_0.08.mt/')
-    ht0 = mt0.cols()
-    mt  = hl.read_matrix_table(gs_bucket+'ukbb31063.GT.chr22.MAF_0.05.white_british.mt/')
-    mt = mt.annotate_cols(PC1 = ht0[mt.s]['PC1'])
-    
-    gwas_ht = gwas(mt, mt.GT.n_alt_alleles(),)
-    
-    
